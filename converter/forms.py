feb0652ff28c097b6ede7ab8319ebbaeb6d7abfc
Isaac Adewumi <72208758+prettyirrelevant@users.noreply.github.com>
2021-10-14 14:19:40 +0100

    Update forms.py

--- converter/forms.py
+++ converter/forms.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
@@ -10,10 +12,10 @@ class PlaylistConverterForm(forms.Form):
         max_length=70,
         help_text=_("A name to call the new playlist."),
     )
-    playlist_id = forms.CharField(
-        label=_("Playlist ID"),
+    playlist_link = forms.URLField(
+        label=_("Playlist Link"),
         max_length=100,
-        help_text=_("The ID of the Playlist to convert."),
+        help_text=_("The link of the playlist to convert."),
     )
     source = forms.ChoiceField(
         label=_("Source"),
@@ -27,10 +29,31 @@ class PlaylistConverterForm(forms.Form):
     )
 
     def clean(self):
+        accepted_domains = ["open.spotify.com", "music.youtube.com", "youtube.com"]
         cleaned_data = super().clean()
+
         source = cleaned_data.get("source")
         target = cleaned_data.get("target")
+
+        playlist_link = cleaned_data.pop("playlist_link")
+        playlist_netloc = urlparse(playlist_link).netloc
+        
+        if playlist_netloc not in accepted_domain:
+            self.add_error(
+                "playlist_link", f"We currently do not support {playlist_netloc}."
+            )
 
         if source == target:
             self.add_error("source", " ")
             self.add_error("target", "Source and target cannot be the same platform.")
+
+        if playlist_netloc in accepted_domains[1:]:
+            cleaned_data.update(
+                {"playlist_id": urlparse(playlist_link).query.split("=")[-1]}
+            )
+        else:
+            cleaned_data.update(
+                {"playlist_id": urlparse(playlist_link).path.split("/")[-1]}
+            )
+
+        return cleaned_data
